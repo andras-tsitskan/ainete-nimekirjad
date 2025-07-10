d3f752e4738eea909a49bff45444f75f1288fdfe
andras-tsitskan <antriiko@gmail.com>
2025-07-10 23:10:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ df = pd.read_sql_query("SELECT * FROM narcotics", conn)
 
 # Remove 'id' column and rename 'drug_name' to 'eestikeelne nimetus'
 df = df.drop(columns=["id"], errors="ignore")
+df = df.drop(columns=["collected_on"], errors="ignore")
+df = df.rename(columns={"category": "nimekiri"})
 df = df.rename(columns={"drug_name": "eestikeelne nimetus"})
+df = df.rename(columns={"english_name": "ingliskeelne nimetus"})
 
 # Page layout
 st.set_page_config(page_title="Narkootilised ja psühhotroopsed ained", layout="wide")
